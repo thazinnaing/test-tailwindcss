096584292b73ac52d5dd240533b339809a4d77eb
Thazin <thazinnaingmm@gmail.com>
2023-11-01 11:36:33 +0630

    hover and focus , focus ring

--- src/components/HoverButton.jsx
+++ src/components/HoverButton.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 const HoverButton = () => {
   return (
-    <div>
-        <button class="bg-sky-400 rounded-full px-3 py-3 text-white hover:bg-sky-600 hover:font-semibold">
-            Save Chages
+    <div class="flex space-x-4">
+        <button class="bg-sky-400 rounded-full px-3 py-3 text-white hover:bg-sky-600 ">
+            Save Hover Chages
         </button>
+        <button class="bg-violet-500 rounded-full px-3 py-3 hover:bg-violet-600 focus:outline-none active:bg-violet-700 focus:ring focus:ring-violet-300">
+            Save Ring Changes
+        </button>
+
       
     </div>
   );
